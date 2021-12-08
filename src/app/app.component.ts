f41973c8f689ddbbdf030d1c5679d38f43c8da02
Fernando Valle <fernandovalle@Fernandos-MacBook-Pro.local>
2021-12-07 21:37:43 -0300

    Requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { AppService } from './app.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'arduino-frontend';
+export class AppComponent implements OnInit {
+    title = 'arduino-frontend';
+    
+    private ngUnsubscribe = new Subject();
+
+    constructor(
+        private appService: AppService
+    ) {}
+
+    ngOnInit() {
+        this.getDados();
+    }
+
+    private getDados() {
+        this.appService.getLogs().pipe(takeUntil(this.ngUnsubscribe)).subscribe((val: any) => { console.log(val) });
+    }
+
+    private getStatus() {
+        this.appService.getStatus().pipe(takeUntil(this.ngUnsubscribe)).subscribe((val: any) => { console.log(val) });
+    }
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AppService {
+
+    constructor(
+        private http: HttpClient
+    ) { }
+
+    public getLogs(): Observable<any> {
+        let url = `https://energy-consumption-catolica.herokuapp.com/api/consumption`;
+        return this.http.get<any>(url);
+    }
+    
+    public getStatus(): Observable<any> {
+        let url = `https://energy-consumption-catolica.herokuapp.com/api/system/`;   
+        return this.http.get<any>(url);
+    }
+}
